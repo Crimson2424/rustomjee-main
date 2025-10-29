3c232c09a350b6a4483ab65e56dd994e87d5cf42
Crimson2424 <priyankmiles@gmail.com>
2025-10-29 17:11:08 +0530

    Nav changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,7 +14,7 @@ const NavigationBar = forwardRef(({
   padding = "p-2 py-2 xl:p-4",
   bgColor = "bg-white",
   textColor = "text-gray-500",
-  position = "absolute bottom-[1vw] left-1/2 -translate-x-1/2",
+  position = "absolute bottom-[0.3vw] left-1/2 -translate-x-1/2",
   showIcons = {
     home: true,
     inventory: true,
@@ -34,48 +34,55 @@ const NavigationBar = forwardRef(({
   return (
     <div
       ref={ref}
-      className={`${position} z-50 opacity-0 flex ${gap} items-center ${padding} rounded-xs ${bgColor} ${textColor} ${className}`}
+      className={`${position} z-50 opacity-0  flex ${gap} xl:gap-15 xl:px-6 items-center ${padding} rounded-xs ${bgColor} ${textColor} ${className}`}
     >
       {showIcons.home && (
         <IoHomeOutline 
+        title="Home"
           onClick={() => handleNavigation('/')}
-          className={`hover:scale-110 hover:-translate-y-2 hover:cursor-pointer transition-all ${iconSize}`} 
+          className={`hover:scale-110 hover:-translate-y-2 hover:cursor-pointer transition-all  ${iconSize}`} 
         />
       )}
       {showIcons.inventory && (
         <MdOutlineInventory 
+        title="Features"
           onClick={() => handleNavigation('/inventory')}
-          className={`hover:scale-110 hover:-translate-y-2 hover:cursor-pointer transition-all ${iconSize}`} 
+          className={`hover:scale-110 hover:-translate-y-2 hover:cursor-pointer transition-all text-xl xl:text-2xl ${iconSize}`} 
         />
       )}
       {showIcons.view360 && (
-        <MdOutline360 
+        <MdOutline360
+        title="Drone views" 
           onClick={() => handleNavigation('/360-view')}
-          className={`hover:scale-110 hover:-translate-y-2 hover:cursor-pointer transition-all ${iconSize}`} 
+          className={`hover:scale-110 hover:-translate-y-2 hover:cursor-pointer transition-all text-xl xl:text-2xl ${iconSize}`} 
         />
       )}
       {showIcons.gallery && (
         <GrGallery 
+        title="Gallery"
           onClick={() => handleNavigation('/gallery')}
-          className={`hover:scale-110 hover:-translate-y-2 hover:cursor-pointer transition-all ${iconSize}`} 
+          className={`hover:scale-110 hover:-translate-y-2 hover:cursor-pointer transition-all text-xl xl:text-2xl ${iconSize}`} 
         />
       )}
       {showIcons.floorPlan && (
         <FloorPlanIcon 
+        title="Floor Plan"
           onClick={() => handleNavigation('/floorplan')}
           className="hover:scale-110 hover:-translate-y-2 hover:cursor-pointer transition-all h-5 xl:h-7" 
         />
       )}
       {showIcons.amenities && (
         <TbStack 
+        title='Amenities'
           onClick={() => handleNavigation('/amenities')}
-          className={`hover:scale-110 hover:-translate-y-2 hover:cursor-pointer transition-all ${iconSize}`} 
+          className={`hover:scale-110 hover:-translate-y-2 hover:cursor-pointer transition-all text-xl xl:text-2xl ${iconSize}`} 
         />
       )}
       {showIcons.location && (
         <LuMapPin 
+        title="Map"
           onClick={() => handleNavigation('/location')}
-          className={`hover:scale-110 hover:-translate-y-2 hover:cursor-pointer transition-all ${iconSize}`} 
+          className={`hover:scale-110 hover:-translate-y-2 hover:cursor-pointer transition-all text-xl xl:text-2xl ${iconSize}`} 
         />
       )}
     </div>
